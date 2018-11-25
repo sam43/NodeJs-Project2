504bcd6d82ba0a933e813a30c91d15d1d3bb5aec
Sayem <sayemruet.103043@gmail.com>
2018-11-25 17:19:20 +0600

    gitignore added

---
+++ app.js
@@ -0,0 +1,39 @@
+
+const port = 3000
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+const apiKey = '393e8f82b7da94f243f5cfbd6f7d1c2a'
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null})
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'})
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'})
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+        res.render('index', {weather: weatherText, error: null})
+      }
+    }
+  });
+})
+
+app.listen(port, function () {
+  console.log("Example app listening on port no : "+port+"!")
+})
